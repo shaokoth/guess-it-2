bbd11f2ab773ad35010c157f8be73fd2505620ff
shaokoth <shadrackomondi005@gmail.com>
2024-10-28 01:54:21 +0300

    fix: improve error handling for edge cases in input data

--- student/main.go
+++ student/main.go
@@ -4,21 +4,15 @@ import (
 	"bufio"
 	"fmt"
 	"log"
-	"math"
 	"os"
 	"strconv"
 
-	"guess-it-2/stats"
+	guess_it "guess-it-2/stats"
 )
 
 func main() {
-	if len(os.Args) != 1 {
-		fmt.Println("Usage: go run .")
-	}
 	scanner := bufio.NewScanner(os.Stdin)
-	xValues := []float64{}
 	yValues := []float64{}
-	count := 1
 	// Loop over each line of input
 	for scanner.Scan() {
 		// Read the value from current line
@@ -27,20 +21,16 @@ func main() {
 		if err != nil {
 			log.Fatal("Error parsing data")
 		} else {
-			yValues = guess_it.MaintainLastFive(yValues, num)
-			xValues = guess_it.MaintainLastFive(xValues, float64(count))
+			yValues = append(yValues, num)
 		}
-		count++
-
-		if err := scanner.Err(); err != nil {
-			fmt.Println("Error", err)
-			return
-		}
+		// Check if there are at least 4 values
 		if len(yValues) > 1 {
-			a, b := guess_it.LinearRegression(xValues, yValues)
-			stdDev := guess_it.StandardDeviation(yValues)
-			lower, upper := guess_it.PredictiveInterval(float64(count), a, b, stdDev)
-			fmt.Println(math.Round(lower), math.Round(upper))
+			lowerLImit, upperLImit := guess_it.Range(yValues)
+			fmt.Printf("%d %d\n", lowerLImit, upperLImit)
 		}
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error", err)
+		return
 	}
 }
